472f73edcfc3c76aa4581c0bdf740150217d05c6
182453972 <44743506+182453972@users.noreply.github.com>
2022-04-10 10:00:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' feeds/luci/collections/luci/Makefile
+#修改主机名
+sed -i 's/OpenWrt/Bin-Lean/g' package/base-files/files/bin/config_generate
+#关闭自建私有源签名验证
+sed -i '90d' package/system/opkg/Makefile
+#修改在线安装插件私有源
+sed -i '/lede/ { s/downloads/https\:\/\/downloads/g }' package/lean/default-settings/files/zzz-default-settings
+sed -i '/lede/ { s/mirrors/https\:\/\/mirrors/g }' package/lean/default-settings/files/zzz-default-settings
+sed -i '/lede/ { s/org/org\/snapshots/g }' package/lean/default-settings/files/zzz-default-settings
+sed -i '/openwrt_luci/ { s/sed/# sed/g; }'  package/lean/default-settings/files/zzz-default-settings
+sed -i 's#https://mirrors.cloud.tencent.com/lede#http://256pd.top:9666/x64/bin#g' package/lean/default-settings/files/zzz-default-settings
+#修改固件名显示内核版本
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=k$(LINUX_VERSION)-/g' include/image.mk
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
